b8fe5636ab63cc19865aa9024c1901c4b70dff79
Amine Ifakkir <Amineifakkir@hotmail.com>
2022-11-04 10:37:40 +0100

    Initial commit

---
+++ GestionPov/src/test/java/com/gestionDePov/GestionPov/GestionPovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionDePov.GestionPov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
